97c101dcb0f50ca41b8c9042f135d92d204bd0b2
Samuel Odongo <samieodd@gmail.com>
2022-02-07 19:03:05 +0300

    Extracted theme switcher widget

--- lib/home.dart
+++ lib/home.dart
@@ -20,31 +20,42 @@ class Home extends StatelessWidget {
                 padding: const EdgeInsets.all(10.0),
                 child: Column(),
               ),
-              Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Container(
-                  width: 130,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    color: Colors.white,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      ThemeButton(
-                        icon: Icons.light_mode,
-                      ),
-                      ThemeButton(
-                        icon: Icons.dark_mode,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              const ThemeSwicther(),
               const CalculationDisplay(),
               const ButtonArea(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ThemeSwicther extends StatelessWidget {
+  const ThemeSwicther({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        width: 130,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40),
+          color: Colors.white,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            ThemeButton(
+              icon: Icons.light_mode,
+            ),
+            ThemeButton(
+              icon: Icons.dark_mode,
+            ),
+          ],
         ),
       ),
     );
